7a951c588880b03dfe067752f4df05f63885ec69
marconeri70 <marconeri70@gmail.com>
2026-09-02 17:45:37 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='scopa-v1';
-const ASSETS=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
